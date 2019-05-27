db27a68de9ded25c49090e25b5049b33f11c2a76
Julian Psotta <psotta@stud.uni-heidelberg.de>
2019-05-27 14:52:42 +0200

    building valhalla with elevation

--- Ubuntu/18.04/Valhalla/configure_valhalla.sh
+++ Ubuntu/18.04/Valhalla/configure_valhalla.sh
@@ -7,13 +7,23 @@ mkdir -p valhalla_tiles
 curl -O $3
 # Assign the file name of the osm extract for later used
 tile_file=$(ls -t | head -n1)
-# Build the config file to the config path
-valhalla_build_config --mjolnir-tile-dir ${PWD}/valhalla_tiles --mjolnir-tile-extract ${PWD}/valhalla_tiles.tar --mjolnir-timezone ${PWD}/valhalla_tiles/timezones.sqlite --mjolnir-admin ${PWD}/valhalla_tiles/admins.sqlite > $2
+# Check for bounding box
+if [[ "$4" != 0 ]] && [[ "$5" != 0 ]] && [[ "$6" != 0 ]] && [[ "$7" != 0 ]]; then
+    echo "Adding elevation";
+    # Build the elevation data
+    valhalla_build_elevation $4 $5 $6 $7 ${PWD}/elevation_tiles;
+    # Build the config file with elevation
+    valhalla_build_config --mjolnir-tile-dir ${PWD}/valhalla_tiles --mjolnir-tile-extract ${PWD}/valhalla_tiles.tar --mjolnir-timezone ${PWD}/valhalla_tiles/timezones.sqlite --mjolnir-admin ${PWD}/valhalla_tiles/admins.sqlite --additional-data-elevation ${PWD}/elevation_tiles > $2;
+else 
+    echo "Skipping elevation";
+    # Build the config file without elevation
+    valhalla_build_config --mjolnir-tile-dir ${PWD}/valhalla_tiles --mjolnir-tile-extract ${PWD}/valhalla_tiles.tar --mjolnir-timezone ${PWD}/valhalla_tiles/timezones.sqlite --mjolnir-admin ${PWD}/valhalla_tiles/admins.sqlite > $2;
+fi
 # Build the admin regions
 valhalla_build_admins --config $2 $tile_file
 # Build the time zones
 ./valhalla_build_timezones $2
 # Finally build the tiles
 valhalla_build_tiles -c $2 $tile_file
-# Tar up the tiles in the valhalla_tiles folder 
+# Tar up the tiles in the valhalla_tiles folder
 find valhalla_tiles | sort -n | tar cf "valhalla_tiles.tar" --no-recursion -T -
