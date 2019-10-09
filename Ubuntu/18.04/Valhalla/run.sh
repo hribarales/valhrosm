6b98522566b8243e34cb0856874f71d1a5471b05
Julian Psotta <julian@gis-ops.com>
2019-10-09 22:05:30 +0200

    #2 add possibility to use custom config file for starting
    Signed-off-by: Julian Psotta <julian@gis-ops.com>

--- Ubuntu/18.04/Valhalla/run.sh
+++ Ubuntu/18.04/Valhalla/run.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
-valhalla_service /conf/valhalla.json 1
+if test -f "/custom_conf/valhalla.json"; then
+   echo "Found existing config file. Using it instead of a new one. Use at your own risk!";
+   valhalla_service /custom_conf/valhalla.json 1;
+else
+    echo "No custom config found. Using the regular config.";
+    valhalla_service /conf/valhalla.json 1;
+fi
+
 # Keep docker running easy
 exec "$@";
